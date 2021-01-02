19901585f107a419e1df7c0968112e070b1fe02f
tniu <tniu@infomedia.com.au>
2021-01-02 20:46:29 +1100

    init

---
+++ hello_world/app.py
@@ -0,0 +1,50 @@
+import json
+import ptvsd
+
+ptvsd.enable_attach(address=('0.0.0.0', 5890), redirect_output=True)
+ptvsd.wait_for_attach()
+
+# import requests
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+
+    # try:
+    #     ip = requests.get("http://checkip.amazonaws.com/")
+    # except requests.RequestException as e:
+    #     # Send some context about this error to Lambda Logs
+    #     print(e)
+
+    #     raise e
+
+    print('test')
+
+    print('something else')
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "message": "hello world11",
+            # "location": ip.text.replace("\n", "")
+        }),
+    }
